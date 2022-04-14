012cf72ecac18353ecec317b88d8780cb35d5a72
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-04-14 11:38:36 +0300

    First commit. Start the homePay project.

---
+++ src/main/java/com/demchenko/homepay/HomePayApplication.java
@@ -0,0 +1,13 @@
+package com.demchenko.homepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomePayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demchenko/homepay/HomePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demchenko.homepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
