cf47a864969c868d80f85c2011ad0acf1e5de32e
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-04-19 13:48:39 +0300

    Added basic level entity.

---
+++ src/main/java/com/demchenko/homepay/entity/basic/Person.java
@@ -0,0 +1,20 @@
+package com.demchenko.homepay.entity.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+}
