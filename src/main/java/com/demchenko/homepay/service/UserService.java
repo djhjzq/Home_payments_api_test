805bb5c9008ac0d0b24ae92d1f0f7f0265afcf4c
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-04-28 11:52:20 +0300

    Simple User level.

--- src/main/java/com/demchenko/homepay/service/UserService.java
+++ src/main/java/com/demchenko/homepay/service/UserService.java
@@ -1,18 +1,6 @@
 package com.demchenko.homepay.service;
 
-import com.demchenko.homepay.entity.User;
-
-import java.util.List;
-
 
 public interface UserService {
 
-    void createUser(String firstName, String lastName, String email,
-                    String password);
-
-    List<User> findAllUsers();
-
-    User findUserById(Long userId);
-
-    User findUserByEmail(String email);
 }
--- src/main/java/com/demchenko/homepay/service/impl/UserServiceImpl.java
+++ src/main/java/com/demchenko/homepay/service/impl/UserServiceImpl.java
@@ -1,13 +1,10 @@
 package com.demchenko.homepay.service.impl;
 
-import com.demchenko.homepay.entity.User;
 import com.demchenko.homepay.repository.UserRepository;
 import com.demchenko.homepay.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,31 +16,4 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
-    @Override
-    public void createUser(String firstName, String lastName,
-                           String email, String password) {
-        User user = new User();
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setEmail(email);
-        user.setPassword(password);
-        userRepository.save(user);
-    }
-
-    @Override
-    public List<User> findAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @Override
-    public User findUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User with this id not found"));
-    }
-
-    @Override
-    public User findUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }
 }
