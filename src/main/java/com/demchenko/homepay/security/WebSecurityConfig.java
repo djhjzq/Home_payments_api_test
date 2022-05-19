8f1ef967096c2eb69e1632185f61ee4154ff83fc
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-05-19 18:26:58 +0300

    security config fixed.

--- src/main/java/com/demchenko/homepay/security/WebSecurityConfig.java
+++ src/main/java/com/demchenko/homepay/security/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .antMatchers("/").permitAll()
+                .antMatchers("/*").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
