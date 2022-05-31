eac412fcd2fe2949772790b32db167f80687d9fa
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-06-01 02:54:29 +0300

    reworked EstateRegistryForm

--- src/main/java/com/demchenko/homepay/dto/request/EstateRegistryForm.java
+++ src/main/java/com/demchenko/homepay/dto/request/EstateRegistryForm.java
@@ -3,9 +3,11 @@ package com.demchenko.homepay.dto.request;
 
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
-public record EstateRegistryForm(@Positive Long userId, @Positive Long cityId, @Positive Long streetId,
+public record EstateRegistryForm(@Positive Long userId, @Size(min = 2, max = 20) String cityName, @Size(min = 2, max = 20)
+                                 String streetName,
                                  @Positive Integer houseNumber, @PositiveOrZero Integer flatNumber) implements Serializable {
 
 }
