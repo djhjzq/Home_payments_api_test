81b96be4b37ba967d51b11ef0bde2c4f2bef2492
Dmitriy <demchenko11.dmitriy@gmail.com>
2022-05-31 21:54:47 +0300

    Hello page for user dashboard.

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,3 +1,32 @@
 $(document).ready(function (){
 
+    getUser();
+
+    function getUser() {
+        let userId = check_cookie_name("Id");
+        $.ajax({
+            url:"/api/user/"+userId,
+            type: "GET",
+            success: function (data) {
+                $("#welcome-text").append(data["firstName"]+" "+
+                data["lastName"]+".");
+            }
+        })
+    }
+
+
+    $("#logout-button").click(function () {
+        window.location.href="/api/logout"
+    })
+
+    function check_cookie_name(name)
+    {
+        let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+        if (match) {
+            return match[2];
+        }
+        else{
+            console.log('--something went wrong---');
+        }
+    }
 });
